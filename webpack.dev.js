2203eb860e9a88b4bae27106b41e4efce789340d
rahul-george <34202955+rahul-george@users.noreply.github.com>
2025-10-20 11:25:02 +0200

    chore: webpack-merge added and configurations adjusted

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,34 +1,10 @@
 // webpack.dev.js
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-
-module.exports = {
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+module.exports = merge(common, {
   mode: "development",
-  entry: "./src/index.js",
-  output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "dist"),
-    clean: true,
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
   },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: "./src/template.html",
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.html$/i,
-        loader: "html-loader",
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
-};
+});
